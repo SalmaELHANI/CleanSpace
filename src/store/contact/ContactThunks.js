fc734329ed0ec63c1fa7739291b40282877f6af0
SALMA EL HANI <salmaelhani7@gmail.com>
2024-03-25 12:44:40 +0000

    Update dotenv

--- src/store/contact/ContactThunks.js
+++ src/store/contact/ContactThunks.js
@@ -1,5 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 const apiUrl = process.env.API_URL || 'http://localhost:3000';
 
 export const createContact = createAsyncThunk(
